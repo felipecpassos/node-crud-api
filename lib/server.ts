678c9090ad1217c65a785f8fe03b5ed389f5d700
felipecpassos <felipe.carvalho.passos@gmail.com>
2021-04-01 18:55:36 -0300

    Oops 2

--- lib/server.ts
+++ lib/server.ts
@@ -6,22 +6,3 @@ app.listen(PORT, () => {
    console.log('Express server listening on port ' + PORT);
 })
 
-// const MongoClient = require('mongodb').MongoClient;
-
-// const uri = "mongodb+srv://user:user@cluster0.9hbnr.mongodb.net/test?retryWrites=true&w=majority"
-
-// MongoClient.connect(
-//     uri, 
-//     {
-//     useUnifiedTopology: true,
-//     useNewUrlParser: true,
-//     },
-//     (err, client) => {
-//     if (err) return console.log(err)
-//     db = client.db('test')
-
-//     app.listen(3000, function(){
-//         console.log('Server is running on port 3000...')
-//     });
-// })
-
