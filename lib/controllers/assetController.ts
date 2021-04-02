c550d10e7abfe422450b057acaa6551a36fa4de4
Felipe <felipe.carvalho.passos@gmail.com>
2021-04-02 17:44:53 -0300

    correcting commit

--- lib/controllers/assetController.ts
+++ lib/controllers/assetController.ts
@@ -1,53 +1,53 @@
 import { Request, Response } from 'express';
 import { insufficientParameters, mongoError, successResponse, failureResponse } from '../modules/common/service';
+import { IAsset } from '../modules/assets/model';
+import AssetService from '../modules/assets/service';
 import { IUnit } from '../modules/units/model';
 import UnitService from '../modules/units/service';
-import { ICompany } from '../modules/companies/model';
-import CompanyService from '../modules/companies/service';
 import e = require('express');
 
-export class UnitController {
+export class AssetController {
 
+    private asset_service: AssetService = new AssetService();
     private unit_service: UnitService = new UnitService();
-    private company_service: CompanyService = new CompanyService();
 
-    public create_unit(req: Request, res: Response) {
+    public create_asset(req: Request, res: Response) {
         // this check whether all the filds were send through the erquest or not
-        if (req.body.unit_name &&
-            req.body.owner_company && 
-            req.body.contact_email &&
-            req.body.phone_number){
-            const unit_params: IUnit = {
-                unit_name: req.body.unit_name,
-                owner_company: req.body.owner_company,
-                contact_email: req.body.contact_email,
-                phone_number: req.body.phone_number,
+        if (req.body.name &&
+            req.body.description && 
+            req.body.unit &&
+            req.body.responsable){
+            const asset_params: IAsset = {
+                name: req.body.name,
+                description: req.body.description,
+                unit: req.body.unit,
+                responsable: req.body.responsable,
                 modification_notes: [{
                     modified_on: new Date(Date.now()),
                     modified_by: null,
-                    modification_note: 'New unit created'
+                    modification_note: 'New asset created'
                 }]
             };
-            this.unit_service.createUnit(unit_params, (err: any, unit_data: IUnit) => {
+            this.asset_service.createAsset(asset_params, (err: any, asset_data: IAsset) => {
                 if (err) {
                     mongoError(err, res);
                 } else {
                     //TODO ADD UNIT ID TO COMPANY DOCUMENT
-                    this.company_service.filterCompany({_id: req.body.owner_company}, (err: any, company_data: ICompany) => {
+                    this.unit_service.filterUnit({_id: req.body.unit}, (err: any, unit_data: IUnit) => {
                         if (err) {
                             mongoError(err, res);
                         } else {
-                            // successResponse('get company successfull', company_data, res);
-                            console.log("get company : "+company_data);
+                            // successResponse('get unit successfull', unit_data, res);
+                            console.log("get unit : "+unit_data);
                             var mongoose = require('mongoose');
-                            var unitObjectId = mongoose.Types.ObjectId(unit_data._id);
-                            company_data.business_units.push(unitObjectId);
-                            this.company_service.updateCompany(company_data, (err: any) => {
+                            var assetObjectId = mongoose.Types.ObjectId(asset_data._id);
+                            unit_data.assets.push(assetObjectId);
+                            this.unit_service.updateUnit(unit_data, (err: any) => {
                                 if (err) {
                                     mongoError(err, res);
                                 } else {
-                                    // successResponse('update company successfull', null, res);
-                                    successResponse('create unit successfull', unit_data, res);
+                                    // successResponse('update unit successfull', null, res);
+                                    successResponse('create asset successfull', asset_data, res);
                                 }
                             });
                         }
@@ -60,15 +60,15 @@ export class UnitController {
         }
     }
 
-    public get_unit(req: Request, res: Response) {
+    public get_asset(req: Request, res: Response) {
         if (req.params.id) {
-            const unit_filter = { _id: req.params.id };
-            this.unit_service.filterUnit(unit_filter, (err: any, unit_data: IUnit) => {
+            const asset_filter = { _id: req.params.id };
+            this.asset_service.filterAsset(asset_filter, (err: any, asset_data: IAsset) => {
                 if (err) {
                     mongoError(err, res);
                 } else {
-                    console.log("get unit : "+unit_data);
-                    successResponse('get unit successfull', unit_data, res);
+                    console.log("get asset : "+asset_data);
+                    successResponse('get asset successfull', asset_data, res);
                 }
             });
         } else {
@@ -76,50 +76,49 @@ export class UnitController {
         }
     }
 
-    // public get_all_units_from_user(req: Request, res: Response) {
-    //         this.unit_service.getAllUnitsFromCompany(req.params.company_id, (err: any, unit_data: [IUnit]) => {
+    // public get_all_assets_from_user(req: Request, res: Response) {
+    //         this.asset_service.getAllAssetsFromUnit(req.params.unit_id, (err: any, asset_data: [IAsset]) => {
     //             if (err) {
     //                 mongoError(err, res);
     //             } else {
-    //                 successResponse('get unit successfull', unit_data, res);
+    //                 successResponse('get asset successfull', asset_data, res);
     //             }
     //         });
     // }
 
-    public update_unit(req: Request, res: Response) {
+    public update_asset(req: Request, res: Response) {
         if (req.params.id &&
-            req.body.unit_name ||
-            req.body.owner_company || 
+            req.body.asset_name ||
+            req.body.owner_unit || 
             req.body.contact_email ||
             req.body.phone_number) {
-            const unit_filter = { _id: req.params.id };
-            this.unit_service.filterUnit(unit_filter, (err: any, unit_data: IUnit) => {
+            const asset_filter = { _id: req.params.id };
+            this.asset_service.filterAsset(asset_filter, (err: any, asset_data: IAsset) => {
                 if (err) {
                     mongoError(err, res);
-                } else if (unit_data) {
-                    unit_data.modification_notes.push({
+                } else if (asset_data) {
+                    asset_data.modification_notes.push({
                         modified_on: new Date(Date.now()),
                         modified_by: null,
-                        modification_note: 'Unit data updated'
+                        modification_note: 'Asset data updated'
                     });
-                    const unit_params: IUnit = {
+                    const asset_params: IAsset = {
                         _id: req.params.id,
-                        unit_name: req.body.unit_name,
-                        owner_company: req.body.owner_company,
-                        contact_email: req.body.contact_email,
-                        phone_number: req.body.phone_number,
-                        is_deleted: req.body.is_deleted ? req.body.is_deleted : unit_data.is_deleted,
-                        modification_notes: unit_data.modification_notes
+                        name: req.body.name,
+                        description: req.body.description,
+                        unit: req.body.unit,
+                        responsable: req.body.responsable,
+                        modification_notes: asset_data.modification_notes
                     };
-                    this.unit_service.updateUnit(unit_params, (err: any) => {
+                    this.asset_service.updateAsset(asset_params, (err: any) => {
                         if (err) {
                             mongoError(err, res);
                         } else {
-                            successResponse('update unit successfull', null, res);
+                            successResponse('update asset successfull', null, res);
                         }
                     });
                 } else {
-                    failureResponse('invalid unit', null, res);
+                    failureResponse('invalid asset', null, res);
                 }
             });
         } else {
@@ -127,40 +126,40 @@ export class UnitController {
         }
     }
 
-    public delete_unit(req: Request, res: Response) {
+    public delete_asset(req: Request, res: Response) {
         if (req.params.id) {
-            this.unit_service.filterUnit({_id: req.params.id}, (err: any, unit_data: IUnit) => {
+            this.asset_service.filterAsset({_id: req.params.id}, (err: any, asset_data: IAsset) => {
                 if (err) {
                     mongoError(err, res);
                 } else {
-                    console.log("get unit : "+unit_data);
-                    this.company_service.filterCompany({_id: unit_data.owner_company}, (err: any, company_data: ICompany) => {
+                    console.log("get asset : "+asset_data);
+                    this.unit_service.filterUnit({_id: asset_data.owner_unit}, (err: any, unit_data: IUnit) => {
                         if (err) {
                             mongoError(err, res);
                         } else {
-                            // successResponse('get company successfull', company_data, res);
-                            console.log("get company : "+[company_data.business_units]);
+                            // successResponse('get unit successfull', unit_data, res);
+                            console.log("get unit : "+[unit_data.business_assets]);
                             //REMOVE COMPANY FROM COMPANIES ARRAY ON USER OBJECT
-                            for (let i = 0; i < company_data.business_units.length; i++){
-                                const unit = company_data.business_units[i];
-                                if (unit.toString() === req.params.id){
-                                    company_data.business_units.splice(i--, 1);
+                            for (let i = 0; i < unit_data.business_assets.length; i++){
+                                const asset = unit_data.business_assets[i];
+                                if (asset.toString() === req.params.id){
+                                    unit_data.business_assets.splice(i--, 1);
                                 }
                             }
-                            console.log("Deleted unit from company unit array: "+[company_data.business_units]);
-                            this.company_service.updateCompany(company_data, (err: any) => {
+                            console.log("Deleted asset from unit asset array: "+[unit_data.business_assets]);
+                            this.unit_service.updateUnit(unit_data, (err: any) => {
                                 if (err) {
                                     mongoError(err, res);
                                 } else {
-                                    // successResponse('update company successfull', null, res);
-                                    //Finally, delete the company itself
-                                    this.unit_service.deleteUnit(req.params.id, (err: any, delete_details) => {
+                                    // successResponse('update unit successfull', null, res);
+                                    //Finally, delete the unit itself
+                                    this.asset_service.deleteAsset(req.params.id, (err: any, delete_details) => {
                                         if (err) {
                                             mongoError(err, res);
                                         } else if (delete_details.deletedCount !== 0) {
-                                            successResponse('delete unit successfull', null, res);
+                                            successResponse('delete asset successfull', null, res);
                                         } else {
-                                            failureResponse('invalid unit', null, res);
+                                            failureResponse('invalid asset', null, res);
                                         }
                                     });
                                     
